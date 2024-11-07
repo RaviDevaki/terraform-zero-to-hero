ef4a77dfd20603f798f8824de5f8755cecde4fc2
Ravi <ravidevaki@gmail.com>
2024-11-07 22:42:56 +0530

    main.tf update

--- Custom/main.tf
+++ Custom/main.tf
@@ -1,14 +1,23 @@
 provider "aws" {
-  region = "us-east-1" # specify the region.. confirms all fixed
+  region = "us-east-1" # specify the region
 }
 
-data "aws_vpc" "VPCTerraform" {
-  filter {
-    name   = "tag:Name"
-    values = ["VPCTerraform"] # replace with your VPC name tag
-  }
+
+# Fetch security group details
+data "aws_security_group" "securitygroupid" {
+  name   = "launch-wizard-6"  # Replace with your security group name
+  #vpc_id = "your-vpc-id"               # Optional: Replace with your VPC ID if needed
 }
 
-output "vpc_id" {
-  value = data.aws_vpc.VPCTerraform.id
+output "security_group_id" {
+  value = data.aws_security_group.securitygroupid.id
+}
+
+resource "aws_instance" "TestServer" {
+  ami = "ami-0eaf7c3456e7b5b68"
+  instance_type = "t2.micro"
+  subnet_id = "subnet-001d928ebed7a3bf9"
+  key_name = "Layer01"
+  security_groups = [data.aws_security_group.securitygroupid.id]
+
 }
